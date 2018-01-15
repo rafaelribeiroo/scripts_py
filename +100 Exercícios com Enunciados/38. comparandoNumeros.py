7139dbfc8ffa81454c3c6d3ba29f7353a4dccd88
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-01-15 21:46:01 -0200

    Corrigindo eexercício 38

--- +100 Exercícios com Enunciados/38. comparandoNumeros.py
+++ +100 Exercícios com Enunciados/38. comparandoNumeros.py
@@ -6,7 +6,7 @@ num1 = int(input('Primeiro número: '))
 num2 = int(input('Segundo número: '))
 if num1 > num2:
     maior = num1
-    print('O PRIMEIRO valor é maior'.format(maior))
+    print('O PRIMEIRO valor é maior')
 elif num2 > num1:
     maior = num2
     print('O SEGUNDO valor é maior')
