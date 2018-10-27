3437bbfd6640da3c1b1d4c7999ed2020e3f130a1
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-10-27 02:06:15 -0300

    Outros exercicios

--- +100 exercícios com enunciados/55. Maior, menor sequencial.py
+++ +100 exercícios com enunciados/55. Maior, menor sequencial.py
@@ -1,12 +1,19 @@
 # 55. Faça um programa que leia o peso de 5 pessoas. No final, mostre
 # qual foi o maior e o menor peso lidos.
-maior = 0
-menor = 999999
+menor, maior = 0, 0
 for c in range(1, 5 + 1, 1):
     peso = float(input('Digite o peso da {}ª pessoa: '.format(c)))
-    if peso > maior:
+    # Se for o primeiro laço, o menor e o maior vão ser congruentes.
+    if c == 1:
         maior = peso
-    if peso < menor:
         menor = peso
+    # Se for os demais laços
+    else:
+        # Caso o peso for maior que maior
+        if peso > maior:
+            maior = peso
+        # Caso o peso for menor
+        if peso < menor:
+            menor = peso
 print('O maior peso lido foi de {}Kg'.format(maior))
 print('O menor peso lido foi de {}Kg'.format(menor))
