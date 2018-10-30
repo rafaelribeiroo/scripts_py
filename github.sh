1c7605a1b89e477e2cad58a873cd1dbd18f252d6
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-10-30 01:01:24 -0300

    Script automação github

--- github.sh
+++ github.sh
@@ -1,5 +1,16 @@
-#!/bin/bash  
-git add .  
-read -p "Commit description: " desc  
-git commit -m "$desc"
-git push origin master
+#!/bin/bash
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+white=`tput setaf 7`
+
+if [ -n "$(git status --porcelain)" ]; then
+	echo "${green}Há alterações que precisam ser 'deployadas'${yellow}"
+	git status --porcelain
+	git add -A .
+	echo "${green}Acima, os arquivos que precisam ser upados para que possa descrever melhor o commit${white}"
+	read -p "Descrição do commit: " desc  
+	git commit -m "$desc"
+	git push origin master
+else
+	echo "Não há alterações nesse repositório, vamos trabalhar!";
+fi
