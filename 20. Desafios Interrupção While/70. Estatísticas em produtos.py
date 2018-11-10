cc845b907fef5d432f88df4198a62defe73a6044
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-11-10 12:39:46 -0200

    Enunciado exercício 70 e teorias alteradas

---
+++ 20. Desafios Interrupção While/70. Estatísticas em produtos.py
@@ -0,0 +1,5 @@
+# 70. Crie um programa que leia o nome e o preço de vários produtos. O programa
+# deverá perguntar se o usuário vai continuar. No final, mostre:
+# > Qual é o total gasto na compra.
+# > Quantos produtos custam mais de R$ 1000.
+# > Qual é o nome do produto mais barato.
