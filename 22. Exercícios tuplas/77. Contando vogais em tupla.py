1c1fdf0a901c745fe17d6fdd55374ad9cd25cfe2
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-11-16 16:56:04 -0200

    77 a ser resolvido, não está mostrando as vogais

--- 22. Exercícios tuplas/77. Contando vogais em tupla.py
+++ 22. Exercícios tuplas/77. Contando vogais em tupla.py
@@ -1,8 +1,9 @@
 # 77. Crie um programa que tenha uma tupla com várias palavras (não usar
 # acentos). Depois disso, você deve mostrar, para cada palavra, quais
 # são as suas vogais.
-palavas = (
+palavras = (
     'aprender', 'programar', 'linguagem', 'python',
     'curso', 'gratis', 'estudar', 'praticar',
     'trabalhar', 'mercado', 'programador', 'futuro'
 )
+print(f'Na palavra {palavras[0].upper()} temos')
