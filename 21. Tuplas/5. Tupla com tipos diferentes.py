1b484263a2cb375a4e9a387a6caabf5a8a1ef1cb
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-11-16 01:27:55 -0200

    72 pelo tutor, arquivo 5 corrigido pela PEP-8

--- 21. Tuplas/5. Tupla com tipos diferentes.py
+++ 21. Tuplas/5. Tupla com tipos diferentes.py
@@ -2,5 +2,6 @@ pessoa = ('Rafael', 24, 'M', 80.56)
 # Apagando a tupla da memória
 del(pessoa)
 # Como a tupla é imutável, nós não conseguimos deletar apenas um item
-del(pessoa[0])
-print(pessoa)
+# Rafael no caso
+# del(pessoa[0])
+# print(pessoa)
--- 22. Exercícios tuplas/72. Número por extenso.py
+++ 22. Exercícios tuplas/72. Número por extenso.py
@@ -2,16 +2,15 @@
 # uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e
 # mostrá-lo por extenso.
-extenso = (
+cont = (
     'zero', 'um', 'dois', 'três', 'quatro', 'cinco',
     'seis', 'sete', 'oito', 'nove', 'dez',
-    'onze', 'doze', 'treze', 'quatorze', 'quinze',
+    'onze', 'doze', 'treze', 'catorze', 'quinze',
     'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte',
 )
-núm = int(input('Digite um número entre 0 e 20: '))
 while True:
-    if núm > 20 or núm < 0:
-        núm = int(input('Tente novamente. Digite um número entre 0 e 20: '))
-    else:
+    núm = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= núm <= 20:
         break
-print('Você digitou o número {}'.format(extenso[núm]))
+    print('Tente novamente. ', end='')
+print('Você digitou o número {}'.format(cont[núm]))
