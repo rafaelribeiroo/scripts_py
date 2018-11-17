939732cb830f9f0a671f8d57e12c1619c9021751
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-11-17 02:03:28 -0200

    Começando a resolver o 1o exercício de lista

---
+++ 24. Desafios Listas I/78. Maior e menor valores na lista.py
@@ -0,0 +1,9 @@
+# 78. Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
+# No final, mostre qual foi o maior e o menor valor digitado e as suas
+# respectivas posições na lista.
+números = list()
+for num in range(0, 5):
+    números.append(int(input(f'Digite um valor para a Posição {num}: ')))
+print('=-' * 30)
+print(f'Você digitou os valores {números}')
+print(f'O maior valor digitado foi {max(números)} nas posições', end=' ')
