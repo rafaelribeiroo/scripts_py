2c7092d0de74651dc400dfc10473c4757dd0713c
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-11-17 01:59:41 -0200

    77 concluido e 75 corrigido pelo professor

--- 22. Exercícios tuplas/75. Análise de dados em uma tupla.py
+++ 22. Exercícios tuplas/75. Análise de dados em uma tupla.py
@@ -3,24 +3,20 @@
 # > Quantas vezes apareceu o valor 9.
 # > Em que posição foi digitado o primeiro valor 3.
 # > Quais foram os números pares.
-c = 0
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-n3 = int(input('Digite mais um número: '))
-n4 = int(input('Digite o último número: '))
-tupla = (
-    n1, n2, n3, n4
+núm = (
+    int(input('Digite um número: ')),
+    int(input('Digite outro número: ')),
+    int(input('Digite mais um número: ')),
+    int(input('Digite o último número: '))
 )
-print(f'Você digitou os valores {tupla}')
-print(f'O valor 9 apareceu {tupla.count(9)} vezes')
-# Um problema do index, é que se ele não encontrar o parâmetro informado ele
-# retorna erro ao invés de 0 ou algo do tipo, então temos uma tratativa
-# nomeada Try/Except para tratar os erros e mostrar uma mensagem mais amigável
-try:
-    print(f'O valor 3 apareceu na {tupla.index(3, 0)+1}ª posição')
-except ValueError:
+print(f'Você digitou os valores {núm}')
+print(f'O valor 9 apareceu {núm.count(9)} vezes')
+# Se houver o número 3 na tupla
+if 3 in núm:
+    print(f'O valor 3 apareceu na {núm.index(3)+1}ª posição')
+else:
     print('O valor 3 não foi digitado em nenhuma posição')
 print('Os valores pares digitados foram ', end='')
-for num in tupla:
-    if num % 2 == 0:
-        print(f'{num} ', end='')
+for n in núm:
+    if n % 2 == 0:
+        print(n, end=' ')
--- 22. Exercícios tuplas/77. Contando vogais em tupla.py
+++ 22. Exercícios tuplas/77. Contando vogais em tupla.py
@@ -7,3 +7,13 @@ palavras = (
     'trabalhar', 'mercado', 'programador', 'futuro'
 )
 print(f'Na palavra {palavras[0].upper()} temos')
+while 'a' in palavras[0]:
+	print('a')
+while 'e' in palavras[0]:
+	print('e')
+while 'i' in palavras[0]:
+	print('i')
+while 'o' in palavras[0]:
+	print('o')
+while 'u' in palavras[0]:
+	print('u')
