173df4545f679cc8ca832142b59e5cc8a0e7c72e
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-12-26 14:59:55 -0200

    Correção de exercícios

--- +100 exercícios com enunciados/57. Validação de dados.py
+++ +100 exercícios com enunciados/57. Validação de dados.py
@@ -1,7 +1,6 @@
 # 57. Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores
 # M ou F. Caso esteja errado, peça a digitação novamente até ter um valor
 # correto.
-sexo = ''
 sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
 # Enquanto sexo não estiver em Masculino nem Feminino
 # Isso só funciona com strings, com números não
--- 26. Exercícios Listas II/84. Lista composta e análise de dados.py
+++ 26. Exercícios Listas II/84. Lista composta e análise de dados.py
@@ -4,20 +4,27 @@
 # > Uma listagem com as pessoas mais pesadas.
 # > Uma listagem com as pessoas mais leves.
 lista = list()
+galera = list()
 c = 0
 resp = ' '
 while resp not in 'N':
     nome = str(input('Nome: '))
-    peso = float(input('Peso: '))
+    peso = str(input('Peso: '))
     if c == 0:
-        pesada = peso
-        leve = peso
+        leve = pesada = peso
     else:
         if peso > pesada:
             pesada = peso
         if peso < leve:
             leve = peso
     c += 1
-    lista.append()
+    lista.append(nome)
+    lista.append(peso)
+    galera.append(lista[:])
+    lista.clear()
     resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
-print(lista)
+print(f'Ao todo, você cadastrou {c} pessoas.')
+print(f'O maior peso foi de {pesada}Kg. Peso de', end=' ')
+for pessoa in enumerate(galera):
+    if pessoa[:][1] == pesada:
+        print(f'{pessoa[:][1]}', end=' ')
