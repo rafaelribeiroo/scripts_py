67cb0b83cc3c08fe2a28e3c873b7d06135153f79
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-05-15 17:02:07 -0300

    Atualização do HE

--- Avulsos/HE.py
+++ Avulsos/HE.py
@@ -1,4 +1,6 @@
-from time import sleep
+# from time import sleep
+
+print('{:*^40}'.format('M A G A Z I N E'))
 
 # Altere os preços aqui, para não ter que mudar em 30 variáveis diferentes
 preço_interodonto = 16.00
@@ -10,9 +12,8 @@ exame_simples = exame_especial = total_exame = 0.00
 terapia = 0.00
 
 # Apenas se o usuário selecionar, incrementará com o valor adequado
-odontoprev = interodonto = gamalu = pat = sindicato = transporte = trans_hb = 0.00
-
-cont1 = cont2 = cont3 = cont4 = cont5 = cont6 = 0
+odontoprev = interodonto = gamalu = pat = sdt = transporte = trans_hb = 0.00
+he_final = somatório = 0
 
 print('*' * 6, 'Simulador de cálculo salarial', '*' * 6, end='\n\n')
 sal = float(input('Salário bruto [. pros centavos]: '))
@@ -69,181 +70,170 @@ while True:
     [  4 ] Grêmio Desportivo: Gamalu
     [  5 ] Contribuição Assistencial (Sindicato)
     [  6 ] PAT - Lei 6.321/76
-    [  7 ] O que significa a opção 6?
-    [  8 ] Vale transporte
-    [  9 ] Estacionamento Agabê
+    [  7 ] Vale transporte
+    [  8 ] Estacionamento Agabê
+    [  9 ] Cálculo de Horas Extras
     ''')
     benefícios = int(input('Digite um de cada vez: '))
     if benefícios < 0:
         print('Saindo do menu, aguarde 3seg.')
-        sleep(3)
+        # sleep(3)
         break
+
     # Condição pronta
     if benefícios == 1:
-        cont1 += 1
-        if cont1 == 1:
-            dep_odonto = int(input('\nAlém de você, há quantos' +
-                                   ' dependentes nesse plano? '))
-            odontoprev = (preço_odontoprev + (preço_odontoprev * dep_odonto))
-            print(f'\nO desconto do odontoprev é de {odontoprev:.2f}')
-            sleep(3)
-        else:
-            print('Você já selecionou esse convênio anteriormente.')
-            sleep(3)
+        dep_odonto = int(input('\nAlém de você, há quantos' +
+                               ' dependentes nesse plano? '))
+        odontoprev = (preço_odontoprev + (preço_odontoprev * dep_odonto))
+        print(f'\nO desconto do odontoprev é de {odontoprev:.2f}')
+        # sleep(3)
+
     # Condição pronta
     elif benefícios == 2:
-        cont2 += 1
-        if cont2 == 1:
-            dep_inter = int(input('\nAlém de você, há quantos' +
-                                  ' dependentes nesse plano? '))
-            interodonto = (preço_interodonto + (preço_interodonto * dep_inter))
-            print(f'\nO desconto da interodonto é de {interodonto:.2f}')
-            sleep(3)
-        else:
-            print('Você já selecionou esse convênio anteriormente.')
-            sleep(3)
+        dep_inter = int(input('\nAlém de você, há quantos' +
+                              ' dependentes nesse plano? '))
+        interodonto = (preço_interodonto + (preço_interodonto * dep_inter))
+        print(f'\nO desconto da interodonto é de {interodonto:.2f}')
+        # sleep(3)
 
     # Condição pronta
     elif benefícios == 3:
-        cont3 += 1
-        if cont3 == 1:
-            dep_unimed = int(input('\nAlém de você, há quantos' +
-                                   ' dependentes nesse plano? '))
-            unimed = (preço_unimed + (preço_unimed * dep_unimed))
+        dep_unimed = int(input('\nAlém de você, há quantos' +
+                               ' dependentes nesse plano? '))
+        unimed = (preço_unimed + (preço_unimed * dep_unimed))
 
-            # If consultas realizadas
-            consulta_bool = str(input('Você ou algum dependente realizou alguma consulta este mês? [s/n] ')).lower().strip()[0]
-            if consulta_bool == 's':
-                consultas = int(input('Informe quantas: '))
-                print('''\n>> Selecione o tipo de consulta:
+        consulta_bool = str(input(
+            'Foi realizado alguma consulta? [s/n] ')
+        ).lower().strip()[0]
+        if consulta_bool == 's':
+            consultas = int(input('Informe quantas: '))
+            print('''\n>> Selecione o tipo de consulta:
 [ 1 ] Consulta em consultório
 [ 2 ] Consulta em pronto socorro
-            ''')
-                for c in range(1, consultas + 1):
-                    tipo_consulta = int(input(f'Informe o tipo da {c}ª consulta: '))
-                    if tipo_consulta == 1:
-                        consulta_consultorio += 29.00
-                    elif tipo_consulta == 2:
-                        consulta_prontosocorro += 34.00
+        ''')
+            for c in range(1, consultas + 1):
+                tipo_consulta = int(
+                    input(f'Informe o tipo da {c}ª consulta: ')
+                )
+                if tipo_consulta == 1:
+                    consulta_consultorio += 29.00
+                elif tipo_consulta == 2:
+                    consulta_prontosocorro += 34.00
 
-            # If exames realizados
-            exame_bool = str(input('Você ou algum dependente realizou algum exame este mês? [s/n] ')).lower().strip()[0]
-            if exame_bool == 's':
-                exames = int(input('Informe quantos: '))
+        exame_bool = str(
+            input('E exame(s)? [s/n] ')
+        ).lower().strip()[0]
+        if exame_bool == 's':
+            qntde_pessoas_examinadas = int(
+                input('Quantas pessoas fizeram exames? ')
+            )
+            for cont1 in range(1, qntde_pessoas_examinadas + 1):
+                exames = int(
+                    input(f'Informe quantos exames a {cont1}ª pessoa fez: ')
+                )
                 print('''\n>> Selecione o tipo de exame:
 [ 1 ] Exame simples
 [ 2 ] Exame especial
-            ''')
-                for c in range(1, exames + 1):
-                    tipo_exame = int(input(f'Selecione o tipo do {c}º exame: '))
+        ''')
+                for cont2 in range(1, exames + 1):
+                    tipo_exame = int(
+                        input(f'Selecione o tipo do {cont2}º exame: ')
+                    )
                     if tipo_exame == 1:
                         exame_simples += 20.00
                     elif tipo_exame == 2:
                         exame_especial += 113.00
                 if exame_simples + exame_especial >= 175.00:
-                    exame_simples = exame_especial = 0.00
-                    total_exame = 175.00
+                    exame_simples = exame_especial = 0
+                    total_exame += 175.00
 
-            # If terapias realizadas
-            terapia_bool = str(input('Você ou algum dependente realizou alguma terapia este mês? [s/n] ')).lower().strip()[0]
-            if terapia_bool == 's':
-                terapias = int(input('Informe quantas: '))
-                terapia = (terapias * 23.00)
-        else:
-            print('Você já selecionou esse convênio anteriormente.')
-            sleep(3)
-        somatório = unimed + consulta_consultorio + consulta_prontosocorro + exame_simples + exame_especial + total_exame + terapia
+        # If terapias realizadas
+        terapia_bool = str(
+            input('E terapia(s)? [s/n] ')
+        ).lower().strip()[0]
+        if terapia_bool == 's':
+            terapias = int(input('Informe quantas: '))
+            terapia = (terapias * 23.00)
+        somatório = unimed + consulta_consultorio + consulta_prontosocorro
+        + exame_simples + exame_especial + total_exame + terapia
         print(f'\nO desconto total da unimed é de {somatório:.2f}')
-        sleep(3)
+        # sleep(3)
 
     # Condição pronta
     elif benefícios == 4:
-        cont4 += 1
-        if cont4 == 1:
-            gamalu = (21.83)
-        else:
-            print('Você já selecionou esse convênio anteriormente.')
-            sleep(3)
+        gamalu = (21.83)
         print(f'\nO desconto do gamalu é de {gamalu:.2f}')
-        sleep(3)
+        # sleep(3)
 
     # Condição pronta
     elif benefícios == 5:
         if sal <= 3500:
-            sindicato = ((sal * 2) / 100)
+            sdt = ((sal * 2) / 100)
         else:
-            sindicato = (70.00)
-        print(f'\nO desconto do sindicato é de {sindicato:.2f}')
-        sleep(3)
+            sdt = (70.00)
+        print(f'\nO desconto do sindicato é de {sdt:.2f}')
+        # sleep(3)
 
     # Condição pronta
     elif benefícios == 6:
-        alimentação = float(input('Ticket alimentação bruto, cheque o horário para descobir: '))
+        alimentação = float(
+            input('Ticket alimentação bruto, cheque o horário para descobir: ')
+        )
         pat = ((alimentação * 1) / 100)
         print(f'\nO desconto do PAT é de {pat:.2f}')
-        sleep(3)
+        # sleep(3)
 
     # Condição pronta
     elif benefícios == 7:
-        print('''Assim, consiste em um programa de alimentação do colaborador, a empresa
-adere a esse programa oferecendo refeitórios próprios ou contratar empresas
-prestadoras de serviços de alimentação coletiva. Pode optar também fornecendo
-vale alimentação ou refeição. Quer saber mais? Basta copiar e colar esse link
-em seu navegador: https://www.youtube.com/watch?v=z11hu8n1phI''')
-
-    # Condição pronta
-    elif benefícios == 8:
-        cont5 += 1
-        if cont5 == 1:
-            transporte = (sal * 6) / 100  # 6% do salário total
-        else:
-            print('Você já selecionou esse convênio anteriormente.')
-            sleep(3)
+        transporte = (sal * 6) / 100  # 6% do salário total
         print(f'\nO desconto do transporte é de {transporte:.2f}')
-        sleep(3)
+        # sleep(3)
 
-    elif benefícios == 9:
-        cont6 += 1
+    elif benefícios == 8:
         print('Você vem de moto ou carro?', end=' ')
         escolha = str(input('')).upper().strip()[0]
-        if cont6 == 1:
-            if escolha == 'M':
-                trans_hb = 25.00
-            elif escolha == 'C':
-                trans_hb = 70.00
-            else:
-                print('Veículo não compreensível')
+        if escolha == 'M':
+            trans_hb = 25.00
+        elif escolha == 'C':
+            trans_hb = 70.00
         else:
-            print('Você já selecionou esse convênio anteriormente.')
-            sleep(3)
+            print('Veículo não compreensível')
         print(f'\nO desconto do estacionamento é de {trans_hb:.2f}')
-        sleep(3)
+        # sleep(3)
 
-print('\nAdm de Pessoal (Saldo banco de horas) → Nesse programa, vou considerar suas HE como 60%')
-he = float(input('Horas extras: '))
-
-# Calculando valor da sua hora extra
-# Pego o valor da hora normal + 60% para obter o valor da minha hora extra
-hora_extra_valor = hora_valor + (hora_valor * 60) / 100
-
-# Cálculo do quanto você ganhará com sua hora extra
-valor_final_he = hora_extra_valor * he
-
-# Somando as receitas (salário + horas_extras)
-sal = sal + valor_final_he
+    elif benefícios == 9:
+        he_qntde = float(input('Quantas horas extras você fez? '))
+        while True:
+            calcular_primeiro = str(
+                input('Escolha qual deseja calcular: [60/100/S]')
+            )
+            if calcular_primeiro == '60':
+                he_60 = float(input('Quantas 60%? '))
+            elif calcular_primeiro == '100':
+                he_100 = float(input('E 100%? '))
+            else:
+                break
+        calculo_60 = (hora_valor * 60) / 100
+        final_60 = calculo_60 * he_60
+        calculo_100 = (hora_valor * 100) / 100
+        final_100 = calculo_100 * he_100
+        he_final = final_60 + final_100
+        print(f'Horas extras vão somar R${he_final:.2f}')
 
 # Cálculo das despesas
-despesas = inss + irpf + odontoprev + interodonto + somatório + gamalu + sindicato + pat + transporte
+despesas = inss + irpf + odontoprev + interodonto + somatório + gamalu + sdt
++ pat + transporte
 
-print(f'\n\nTendo em base que o INSS te custou R$ {inss:.2f}' +
+
+print(f'\n\nSalário Bruto: {sal:.2f}' +
+      f'\nTendo em base que o INSS te custou R$ {inss:.2f}' +
       f'\nIRPF R$ {irpf:.2f}' +
       f'\nOdontoprev R$ {odontoprev:.2f}' +
       f'\nInterodonto R$ {interodonto:.2f}' +
       f'\nUnimed R$ {somatório:.2f}' +
       f'\nGamalu R$ {gamalu:.2f}' +
-      f'\nSindicato R$ {sindicato:.2f}' +
+      f'\nSindicato R$ {sdt:.2f}' +
       f'\nPAT R$ {pat:.2f}' +
       f'\nE transporte R$ {transporte:.2f}' +
-      f'\nO total dá uma soma de R$ {despesas:.2f}')
-
-print(f'Baseado em todas as suas despesas e horas extras (60%), seu salário final será de: R$ {sal - despesas:.2f}')
+      f'\nO total dá uma soma de R$ {despesas:.2f}' +
+      f'\nO total de hora extra equivale a R$ {he_final:.2f}')
