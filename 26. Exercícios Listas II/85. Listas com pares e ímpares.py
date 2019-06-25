d586699a8810c183a9df2ac5401cb41f5c1b1620
Rafael <pereiraribeirorafael@gmail.com>
2019-06-25 16:31:35 -0300

    Exercício 85 corrigido
    Correção pelo instrutor referente exercicio 85

--- 26. Exercícios Listas II/85. Listas com pares e ímpares.py
+++ 26. Exercícios Listas II/85. Listas com pares e ímpares.py
@@ -2,18 +2,16 @@
 # cadastre-os em uma lista única que mantenha separados os valores pares e
 # ímpares. No final, mostre os valores pares e ímpares em ordem crescente
 
-pares_temp = []
-ímpares_temp = []
-principal = list()
-
+núm = [[], []]
+valor = 0
 for contador in range(1, 7 + 1):
-    núm = int(input(f'Digite o {contador}° valor: '))
-    if núm % 2 == 0:
-        pares_temp.append(núm)
+    valor = int(input(f'Digite o {contador}º valor: '))
+    if valor % 2 == 0:
+        núm[0].append(valor)
     else:
-        ímpares_temp.append(núm)
-principal.append(pares_temp)
-principal.append(ímpares_temp)
+        núm[1].append(valor)
 print('-=' * 30)
-print(f'Os valores pares digitados foram: {sorted(principal[0])}')
-print(f'Os valores ímpares digitados foram: {sorted(principal[1])}')
+núm[0].sort()
+núm[1].sort()
+print(f'Os valores pares digitados foram: {núm[0]}')
+print(f'Os valores ímpares digitados foram: {núm[1]}')
