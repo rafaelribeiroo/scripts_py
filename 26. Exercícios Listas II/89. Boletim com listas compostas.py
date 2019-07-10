ba6a1b75482dbbfb13a9e0f98d061104c09b858d
Rafael <pereiraribeirorafael@gmail.com>
2019-07-10 13:21:52 -0300

    Referenciando
    89 incluído no outro diretório também

--- 26. Exercícios Listas II/89. Boletim com listas compostas.py
+++ 26. Exercícios Listas II/89. Boletim com listas compostas.py
@@ -3,35 +3,35 @@
 # de cada um e permita que o usuário possa mostrar as notas de cada aluno
 # individualmente.
 
-resposta = ''
-temp = list()
-principal = list()
-while resposta in 'Ss':
-    # Alimentação
-    temp.append(str(input('Nome: ')))
-    temp.append(float(input('Nota 1: ')))
-    temp.append(float(input('Nota 2: ')))
-    media = (temp[1] + temp[2]) / 2
-    temp.append(media)
-    principal.append(temp[:])
-    temp.clear()
-    resposta = str(input('Quer continuar? [S/n] ')).lower().strip()[0]
-print('-=' * 30)
-print('No. NOME         MÉDIA')
-print('–' * 25)
-for pos, elemento in enumerate(principal):
-    print(f'{pos}', end='')
-    for pos_item, item in enumerate(elemento):
-        if pos_item == 0:
-            print(f'{item:^12}', end='')
-        if pos_item == 3:
-            print(f'{item:>7}')
-print('–' * 30)
+ficha = list()
+resp = ''
 while True:
-    notas = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
-    for pos_lista, lista in enumerate(principal):
-        if notas == pos_lista:
-            print(f'O aluno {principal[notas][0]} tem as notas'
-                  f'{principal[notas][1:3]}')
-    if notas == 999:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    # Podemos utilizar o append da forma abaixo também
+    # A variável composta tem 3 níveis de composição
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
         break
+
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-=' * 26)
+# No índice não vai mostrar as notas individuais, já que a lista entende
+# que uma lista interna é apenas um elemento (1 índice apenas)
+for índice, elemento in enumerate(ficha):
+    # E pulamos ele aqui, por isso o [0] e [2]
+    print(f'{índice:<4}{elemento[0]:<10}{elemento[2]:>8.1f}')
+while True:
+    print('–' * 35)
+    opção = int(input('Mostrar notas de qual aluno? (99 interrompe): '))
+    if opção == 99:
+        print('FINALIZANDO...')
+        break
+    # - 1 porque o primeiro é 0 e o último é n - 1
+    if opção <= len(ficha) - 1:
+        print(f'Notas de {ficha[opção][0]} são {ficha[opção][1]}')
+print('<<< VOLTE SEMPRE >>>')
