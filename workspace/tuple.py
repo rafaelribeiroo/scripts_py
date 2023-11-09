67c8ef465fc24cd1a30ad22477449112237aafc7
Rafael <pereiraribeirorafael@gmail.com>
2023-11-09 18:58:37 -0300

    removendo scripts antigos

--- workspace/tuple.py
+++
@@ -1,8 +0,0 @@
-quotes = (
-    'Ilyena, my love, forgive me!',
-    'A man who trusts everyone is a fool, and a man who trusts no one is a '
-    'fool. We are all fools, if we live long enough.',
-    'What you want is what you cannot have. What you cannot have is what you want.'
-)
-
-print(quotes[1])
